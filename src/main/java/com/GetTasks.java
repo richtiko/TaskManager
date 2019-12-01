f16a65653e7029be96099f8eaaab485340a17591
a <richtiko@gmail.com>
2019-12-01 12:40:28 -0800

    message

--- src/main/java/com/GetTasks.java
+++ src/main/java/com/GetTasks.java
@@ -1,4 +1,4 @@
-package com.taskmanager;
+package com;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
@@ -7,12 +7,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-//@WebServlet("/get")
 public class GetTasks extends HttpServlet {
+    List<String> tasks = new ArrayList<>();
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        tasks.add("aaaa");
+        tasks.add("bbbb");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        resp.getWriter().println("example output");
+        synchronized (tasks) {
+            req.setAttribute("tasks", tasks);
+            req.getRequestDispatcher("/WEB-INF/index.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String task = (String)req.getParameter("taskname");
+        synchronized (tasks) {
+            tasks.add(task);    
+        }
+        resp.sendRedirect(req.getContextPath());
     }
 }
